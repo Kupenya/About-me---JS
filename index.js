64964dc51ccf6a84ce5ff10dcd477d5e6abcf76a
Kupenya <81162281+Kupenya@users.noreply.github.com>
2022-06-24 10:44:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = 'My name is Alphonsus Glory'
+document.getElementById("height").innerHTML = 'I am 5ft 8inches'
+document.getElementById("country").innerHTML = 'The name of my country is Nigeria'
